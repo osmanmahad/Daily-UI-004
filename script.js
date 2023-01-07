a303989716a10fae0025533839d3818f835cf9be
osmanmahad <maosman@uwaterloo.ca>
2023-01-06 20:14:56 -0500

    First Commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const display = document.querySelector("#display");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((item) => {
+  item.onclick = () => {
+    if (item.id == "clear") {
+      display.innerText = "";
+    } else if (item.id == "backspace") {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText != "" && item.id == "equal") {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == "" && item.id == "equal") {
+      display.innerText = "Empty!";
+      setTimeout(() => (display.innerText = ""), 2000);
+    } else {
+      display.innerText += item.id;
+    }
+  };
+});
+
+const date = new Date();
+const twoDigitMinutes = date.toLocaleString("en-us", {
+  hour: "2-digit",
+  minute: "2-digit",
+});
+
+document.getElementById("datetime").innerHTML = twoDigitMinutes;
